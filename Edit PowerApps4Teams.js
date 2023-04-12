9492c2c4cf744643ea5be0e346d41c3fe257e445
MarceLehmann <64711607+MarceLehmann@users.noreply.github.com>
2023-04-12 09:05:43 +0200

    Update Edit PowerApps4Teams.js

--- Edit PowerApps4Teams.js
+++ Edit PowerApps4Teams.js
@@ -1,1 +1,7 @@
-javascript:(function(){  var currentUrl = window.location.href;  var environmentId = currentUrl.match(/environments\/([\w-]+)\//)[1];  var appId = prompt("Bitte fügen Sie die App-ID aus der Zwischenablage ein:");  var newUrl = "https://make.powerapps.com/e/" + environmentId + "/canvas/?action=edit&app-id=%2Fproviders%2FMicrosoft.PowerApps%2Fapps%2F" + appId;  window.open(newUrl, "_blank");})();
+javascript:(function() {
+  var currentUrl = window.location.href;
+  var environmentId = currentUrl.match(/environments\/([\w-]+)\//)[1];
+  var appId = prompt("Please paste the App ID from your clipboard:");
+  var newUrl = "https://make.powerapps.com/e/" + environmentId + "/canvas/?action=edit&app-id=%2Fproviders%2FMicrosoft.PowerApps%2Fapps%2F" + appId;
+  window.open(newUrl, "_blank");
+})();
