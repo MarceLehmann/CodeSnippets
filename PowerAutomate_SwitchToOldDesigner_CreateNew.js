696a4c7f88e43c05ea77c0e5b618f27469ca5af7
MarceLehmann <64711607+MarceLehmann@users.noreply.github.com>
2023-10-20 11:16:53 +0200

    Update and rename PowerAutomate_SwitchToOldDesigner.js to PowerAutomate_SwitchToOldDesigner_CreateNew.js

--- PowerAutomate_SwitchToOldDesigner_CreateNew.js
+++ PowerAutomate_SwitchToOldDesigner_CreateNew.js
@@ -4,6 +4,6 @@ javascript:(function() {
         var newUrl = currentUrl.replace('?v3=true', '?v3=false');
         window.location.href = newUrl;
     } else {
-        alert('The URL does not end with ?v3=true');
+        window.location.href = 'https://make.powerautomate.com/environments/Default-6f5c8419-f724-4deb-b347-fd9b9691ca5f/flows/new';
     }
 })();
